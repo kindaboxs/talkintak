9f75fb83e5e5de4aad1bd4a02d5d074c0777d7fd
MrBoxs <rizkywanda200155@gmail.com>
2025-06-08 13:29:48 +0700

    project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,61 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// @ts-ignore -- no types for this plugin
+import drizzle from "eslint-plugin-drizzle";
+import tseslint from "typescript-eslint";
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+	{
+		ignores: [".next"],
+	},
+	...compat.extends("next/core-web-vitals"),
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		plugins: {
+			drizzle,
+		},
+		extends: [
+			...tseslint.configs.recommended,
+			...tseslint.configs.recommendedTypeChecked,
+			...tseslint.configs.stylisticTypeChecked,
+		],
+		rules: {
+			"@typescript-eslint/array-type": "off",
+			"@typescript-eslint/consistent-type-definitions": "off",
+			"@typescript-eslint/consistent-type-imports": [
+				"warn",
+				{ prefer: "type-imports", fixStyle: "inline-type-imports" },
+			],
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_" },
+			],
+			"@typescript-eslint/require-await": "off",
+			"@typescript-eslint/no-misused-promises": [
+				"error",
+				{ checksVoidReturn: { attributes: false } },
+			],
+			"drizzle/enforce-delete-with-where": [
+				"error",
+				{ drizzleObjectName: ["db", "ctx.db"] },
+			],
+			"drizzle/enforce-update-with-where": [
+				"error",
+				{ drizzleObjectName: ["db", "ctx.db"] },
+			],
+		},
+	},
+	{
+		linterOptions: {
+			reportUnusedDisableDirectives: true,
+		},
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+			},
+		},
+	}
+);
