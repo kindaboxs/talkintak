773a27170127cf7095d4433f09e2cc4eef070353
MrBoxs <rizkywanda200155@gmail.com>
2025-06-14 01:06:15 +0700

    feat: Implement agents module with CRUD operations, forms, and responsive UI components

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,10 +1,39 @@
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import { agentInsertSchema } from "@/modules/agents/schemas";
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { agents } from "@/server/db/schema";
 
 export const agentsRouter = createTRPCRouter({
+	getOne: protectedProcedure
+		.input(z.object({ id: z.string() }))
+		.query(async ({ ctx, input }) => {
+			const [existingAgent] = await ctx.db
+				.select()
+				.from(agents)
+				.where(eq(agents.id, input.id));
+
+			return existingAgent;
+		}),
+
 	getMany: protectedProcedure.query(async ({ ctx }) => {
 		const data = await ctx.db.select().from(agents);
 
 		return data;
 	}),
+
+	create: protectedProcedure
+		.input(agentInsertSchema)
+		.mutation(async ({ ctx, input }) => {
+			const [createAgent] = await ctx.db
+				.insert(agents)
+				.values({
+					...input,
+					userId: ctx.auth.user.id,
+				})
+				.returning();
+
+			return createAgent;
+		}),
 });
